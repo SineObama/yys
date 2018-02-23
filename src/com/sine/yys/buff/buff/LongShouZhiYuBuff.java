ddbc3e1af73730d0987794b3e80d55d1f4c99544
Sine <714186139@qq.com>
2018-02-23 17:02:22 +0800

    分析循环依赖中的循环居然由@see引起，真是垃圾

--- src/com/sine/yys/buff/buff/LongShouZhiYuBuff.java
+++ src/com/sine/yys/buff/buff/LongShouZhiYuBuff.java
@@ -3,13 +3,12 @@ package com.sine.yys.buff.buff;
 import com.sine.yys.buff.BaseIBuff;
 import com.sine.yys.inter.buff.Buff;
 import com.sine.yys.inter.buff.UniqueIBuff;
-import com.sine.yys.skill.LongShouZhiYu;
 import com.sine.yys.util.CallBack;
 
 /**
  * 辉夜姬身上隐藏的龙首之玉buff，代表龙首之玉幻境。
  *
- * @see LongShouZhiYu
+ * @see com.sine.yys.skill.LongShouZhiYu
  */
 public class LongShouZhiYuBuff extends BaseIBuff implements UniqueIBuff, Buff {
     private final CallBack callBack;
