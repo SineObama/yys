75286ef519bcff41630ee2664ec4e9ff48fea885
Sine <714186139@qq.com>
2018-02-24 13:46:17 +0800

    提交测试2

--- src/com/sine/yys/simulation/TestCases.java
+++ src/com/sine/yys/simulation/TestCases.java
@@ -1,10 +1,10 @@
 package com.sine.yys.simulation;
 
+import com.sine.yys.mitama.DiZangXiang;
 import com.sine.yys.mitama.HuoLing;
 import com.sine.yys.mitama.MeiYao;
 import com.sine.yys.mitama.ZhenNv;
-import com.sine.yys.shikigami.GuHuoNiao;
-import com.sine.yys.shikigami.QingXingDeng;
+import com.sine.yys.shikigami.*;
 import com.sine.yys.simulation.component.PropertyImpl;
 
 /**
@@ -22,6 +22,20 @@ public class TestCases {
         red.add(new GuHuoNiao(), new PropertyImpl(attack, 10000, 400, 173, 1, 1.5, 0, 0), new ZhenNv());
         red.add(new QingXingDeng(), new PropertyImpl(attack, 10000, 400, 172, 0, 1.5, 0, 0), new HuoLing());
         blue.add(new QingXingDeng(), new PropertyImpl(attack, 10000, 400, 171, 0.8, 1.5, 3, 0), new MeiYao());
+        blue.add(new GuHuoNiao(), new PropertyImpl(attack, 10000, 400, 170, 0.8, 1.5, 3, 0), new MeiYao());
+        return new RedBlueSimulator(red, blue);
+    }
+
+    /**
+     * 一半胜率
+     */
+    public static RedBlueSimulator test2() {
+        final int attack = 1240;
+        CampInfo red = new CampInfo();
+        CampInfo blue = new CampInfo();
+        red.add(new BiAnHua(), new PropertyImpl(attack, 10000, 400, 173, 1, 1.5, 0, 0), new ZhenNv());
+        red.add(new HuiYeJi(), new PropertyImpl(attack, 10000, 400, 172, 0, 1.5, 0, 0), new HuoLing());
+        blue.add(new BoRe(), new PropertyImpl(attack, 10000, 400, 171, 0.8, 1.5, 0.9, 0), new DiZangXiang());  // 效果命中影响大
         blue.add(new GuHuoNiao(), new PropertyImpl(attack, 10000, 400, 170, 0.8, 1.5, 3, 0), new MeiYao());
         return new RedBlueSimulator(red, blue);
     }
