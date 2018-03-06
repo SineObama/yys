a50ec2a6df6d3108a92627d4324c161312b77fc6
Sine <714186139@qq.com>
2018-03-06 17:28:07 +0800

    临时保存战场鲤鱼旗

--- src/com/sine/yys/simulation/component/BattleKoinobori.java
+++ src/com/sine/yys/simulation/component/BattleKoinobori.java
@@ -1,50 +1,44 @@
 package com.sine.yys.simulation.component;
 
+import com.sine.yys.buff.buff.BattleFlag;
+import com.sine.yys.event.BattleStartEvent;
 import com.sine.yys.inter.Entity;
+import com.sine.yys.inter.EventHandler;
 
 /**
  * 战场鲤鱼旗。
  * 还未实现功能。
  */
-public class BattleKoinobori extends EntityImpl implements Entity {
-    private double damageRatio = 1.0;
-    private double cureRatio = 1.0;
-    private double damageRatioAddition = 0.2;
-    private double cureRatioReduction = 0.2;
+public class BattleKoinobori extends EntityImpl implements Entity, EventHandler<BattleStartEvent> {
+    private int level = 0;
+    private final double damageRatioAddition = 0.15;
+    private final double cureRatioReduction = 0.15;
+    private BattleFlag battleFlag = new BattleFlag(this);
 
     public BattleKoinobori(double speed) {
         super(new PropertyImpl(0, 0, 0, speed, 0, 0, 0, 0), null, null);  // TODO 行动技能：给全体加增伤buff
-    }
-
-    public double getDamageRatio() {
-        return damageRatio;
-    }
-
-    public void setDamageRatio(double damageRatio) {
-        this.damageRatio = damageRatio;
-    }
-
-    public double getCureRatio() {
-        return cureRatio;
-    }
-
-    public void setCureRatio(double cureRatio) {
-        this.cureRatio = cureRatio;
     }
 
     public double getDamageRatioAddition() {
         return damageRatioAddition;
     }
 
-    public void setDamageRatioAddition(double damageRatioAddition) {
-        this.damageRatioAddition = damageRatioAddition;
-    }
-
     public double getCureRatioReduction() {
         return cureRatioReduction;
     }
 
-    public void setCureRatioReduction(double cureRatioReduction) {
-        this.cureRatioReduction = cureRatioReduction;
+    public void levelUp() {
+        level += 1;
+        battleFlag.setDamage(level * damageRatioAddition);
+        battleFlag.setCure(level * cureRatioReduction);
+    }
+
+    public BattleFlag getBattleFlag() {
+        return battleFlag;
+    }
+
+    @Override
+    public void handle(BattleStartEvent event) {
+
     }
 }
