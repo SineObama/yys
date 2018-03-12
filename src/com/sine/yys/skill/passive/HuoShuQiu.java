9cd68ec174b8cc15732c77dcb3f4b6415fa41935
Sine <714186139@qq.com>
2018-03-12 17:06:29 +0800

    修改火鼠裘的监听方式，改为根据龙首之玉开关事件移动监听器。

--- src/com/sine/yys/skill/passive/HuoShuQiu.java
+++ src/com/sine/yys/skill/passive/HuoShuQiu.java
@@ -1,9 +1,8 @@
 package com.sine.yys.skill.passive;
 
-import com.sine.yys.buff.buff.LongShouZhiYuBuff;
 import com.sine.yys.event.BeAttackEvent;
-import com.sine.yys.event.DieEvent;
-import com.sine.yys.event.EnterEvent;
+import com.sine.yys.event.LongShouZhiYuOff;
+import com.sine.yys.event.LongShouZhiYuOn;
 import com.sine.yys.inter.Controller;
 import com.sine.yys.inter.Entity;
 import com.sine.yys.inter.EventHandler;
@@ -14,7 +13,7 @@ import com.sine.yys.util.RandUtil;
  * 辉夜姬-火鼠裘。
  */
 public class HuoShuQiu extends BasePassiveSkill implements PassiveSkill {
-    private final BeAttackHandler ownBeAttackHandler = new BeAttackHandler(false);
+    private final BeAttackHandler beAttackHandler = new BeAttackHandler();
 
     @Override
     public String getName() {
@@ -30,38 +29,18 @@ public class HuoShuQiu extends BasePassiveSkill implements PassiveSkill {
 
     @Override
     public void doInit(Controller controller, Entity self) {
-        self.getEventController().add(new BeAttackHandler(true));
+        self.getEventController().add(beAttackHandler);
+        self.getEventController().add(LongShouZhiYuOn.class, event -> {
+            self.getEventController().remove(beAttackHandler);
+            getOwn().getEventController().add(beAttackHandler);
+        });
+        self.getEventController().add(LongShouZhiYuOff.class, event -> {
+            getOwn().getEventController().remove(beAttackHandler);
+            self.getEventController().add(beAttackHandler);
+        });
     }
 
-    @Override
-    protected EventHandler<EnterEvent> getEnterHandler() {
-        return event -> {
-            getOwn().getEventController().add(ownBeAttackHandler);
-        };
-    }
-
-    @Override
-    public EventHandler<DieEvent> getDieHandler() {
-        return event -> {
-            getOwn().getEventController().remove(ownBeAttackHandler);
-        };
-    }
-
-    /**
-     * 通过参数创建2个实例，分别监听自身被攻击和阵营被攻击。
-     */
     class BeAttackHandler extends SealablePassiveHandler implements EventHandler<BeAttackEvent> {
-        private final boolean flag;
-
-        BeAttackHandler(boolean noHuanJing) {
-            this.flag = noHuanJing;
-        }
-
-        @Override
-        public boolean sealed() {
-            return super.sealed() || (getSelf().getBuffController().contain(LongShouZhiYuBuff.class) ^ flag);
-        }
-
         @Override
         public void handle(BeAttackEvent event) {
             if (RandUtil.success(getPct())) {
