d69f855d747bf711481d515672481e0201611fa6
Sine <714186139@qq.com>
2018-03-14 10:38:01 +0800

    修复减疗buff 虽然还没用过

--- src/com/sine/yys/buff/debuff/ReduceCure.java
+++ src/com/sine/yys/buff/debuff/ReduceCure.java
@@ -4,17 +4,18 @@ import com.sine.yys.buff.BaseCommonIBuff;
 import com.sine.yys.inter.Entity;
 
 /**
- * 减疗
+ * 减疗。
  */
 public class ReduceCure extends BaseCommonIBuff {
-    private final double pct;
+    private final double cure;
 
-    public ReduceCure(int last, String name, Entity src, double pct) {
+    public ReduceCure(int last, String name, Entity src, double cure) {
         super(last, name, src);
-        this.pct = pct;
+        this.cure = -cure;
     }
 
-    public double getPct() {
-        return pct;
+    @Override
+    public double getCure() {
+        return cure;
     }
 }
