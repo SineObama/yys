540aa17d84aebc8120d1a456f6d2bdcd3627ad29
Sine <714186139@qq.com>
2018-03-27 16:06:18 +0800

    补上童女的治疗AI

--- src/com/sine/yys/shikigami/TongNv.java
+++ src/com/sine/yys/shikigami/TongNv.java
@@ -1,5 +1,8 @@
 package com.sine.yys.shikigami;
 
+import com.sine.yys.inter.OperationHandler;
+import com.sine.yys.shikigami.operation.SimpleCureHandler;
+import com.sine.yys.skill.HuaNiaoXiangWen;
 import com.sine.yys.skill.MingZhiXianJi;
 import com.sine.yys.skill.commonattack.YuCi;
 import com.sine.yys.skill.passive.YuYi;
@@ -12,5 +15,10 @@ import java.util.Arrays;
 public class TongNv extends BaseShikigami {
     public TongNv() {
         super(Arrays.asList(new YuCi(), new YuYi(), new MingZhiXianJi()), "童女", 2412);
+    }
+
+    @Override
+    public OperationHandler getAI() {
+        return new SimpleCureHandler<>(HuaNiaoXiangWen.class, 0.6, false);
     }
 }
