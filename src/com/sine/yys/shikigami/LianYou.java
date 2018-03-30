aca00f110d58a8a5d3eb5e33284c86cb82c305f4
Sine <714186139@qq.com>
2018-03-30 15:40:22 +0800

    fix：补充镰鼬AI

--- src/com/sine/yys/shikigami/LianYou.java
+++ src/com/sine/yys/shikigami/LianYou.java
@@ -1,10 +1,15 @@
 package com.sine.yys.shikigami;
 
+import com.sine.yys.inter.*;
+import com.sine.yys.shikigami.operation.AutoOperationHandler;
 import com.sine.yys.skill.XiongDiZhiBan;
 import com.sine.yys.skill.commonattack.PangZou;
 import com.sine.yys.skill.passive.RenDuoShiZhong;
+import com.sine.yys.util.Helper;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 镰鼬。
@@ -12,5 +17,21 @@ import java.util.Arrays;
 public class LianYou extends BaseShikigami {
     public LianYou() {
         super(Arrays.asList(new PangZou(), new RenDuoShiZhong(), new XiongDiZhiBan()), "镰鼬", 2680);
+    }
+
+    @Override
+    public OperationHandler getAI() {
+        return new AutoOperationHandler() {
+            @Override
+            public Operation handle(Entity self, Camp own, Map<ActiveSkill, List<? extends Entity>> map) {
+                for (Entity entity : own.getAllAlive()) {
+                    if (entity.getBuffController().contain(XiongDiZhiBan.Attack.class) || entity.getBuffController().contain(XiongDiZhiBan.EffectDef.class)) {
+                        Helper.findKeyBySubClassAndRemove(map, XiongDiZhiBan.class);
+                        break;
+                    }
+                }
+                return super.handle(self, own, map);
+            }
+        };
     }
 }
