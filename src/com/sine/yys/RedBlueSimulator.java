80219f9d2c6fc4d40c915de4e9ae8c054f91eafa
Sine <714186139@qq.com>
2018-04-19 22:37:28 +0800

    多线程test

--- src/com/sine/yys/RedBlueSimulator.java
+++ src/com/sine/yys/RedBlueSimulator.java
@@ -37,29 +37,40 @@ public class RedBlueSimulator {
         count.put(buleName, 0);
     }
 
+    /**
+     * 单线程模拟。
+     *
+     * @param times 总次数。
+     */
     public void test(int times) {
-        int i = 1;
-        try {
-            start = System.currentTimeMillis();
-            for (; i <= times; i++) {
-                BaseCamp red = new PVPCamp(redName, 3);
-                BaseCamp blue = new PVPCamp(buleName, 3);
-                for (EntityInfo info : redInfo.infos)
-                    red.addEntity(new ShikigamiEntityImpl(ShikigamiFactory.create(info.shiShen), info.property, MitamaFactory.create(info.mitama), redInfo.lifeTimes, ShikigamiFactory.getDefaultName(info.shiShen)));
-                for (EntityInfo info : blueInfo.infos)
-                    blue.addEntity(new ShikigamiEntityImpl(ShikigamiFactory.create(info.shiShen), info.property, MitamaFactory.create(info.mitama), blueInfo.lifeTimes, ShikigamiFactory.getDefaultName(info.shiShen)));
-                Simulator simulator = new Simulator(red, blue, Collections.singletonList(new BattleKoinobori(128.0, red, blue)));
-                while (simulator.step()) ;
-                String winName = simulator.getWin().getName();
-                log.info(Msg.join("测试", i, winName, "胜利"));
-                count.put(winName, count.get(winName) + 1);
-            }
-            end = System.currentTimeMillis();
-            printResult();
-        } catch (Exception e) {
-            log.severe(JSON.format("test case", i));
-            throw e;
+        start = System.currentTimeMillis();
+        new SimulateThread(times).run();
+        end = System.currentTimeMillis();
+        printResult();
+    }
+
+    /**
+     * 多线程模拟。
+     *
+     * @param times  总次数。
+     * @param thread 线程数。
+     */
+    public void test(int times, int thread) {
+        start = System.currentTimeMillis();
+        SimulateThread[] threads = new SimulateThread[thread];
+        for (int i = 0; i < thread; i++) {
+            threads[i] = new SimulateThread(times / thread + (times % thread + i) / thread);
+            threads[i].start();
         }
+        for (SimulateThread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        end = System.currentTimeMillis();
+        printResult();
     }
 
     public int getRedWins() {
@@ -74,5 +85,43 @@ public class RedBlueSimulator {
         final String msg = "result=" + count.get(redName) + ":" + count.get(buleName) + ", time=" + (end - start);
         log.info(msg);
         System.out.println(msg);
+    }
+
+    class SimulateThread extends Thread {
+        final int times;
+
+        SimulateThread(int times) {
+            this.times = times;
+        }
+
+        @Override
+        public void run() {
+            final Map<String, Integer> count2 = new HashMap<>(2);
+            count2.put(redName, 0);
+            count2.put(buleName, 0);
+            int i = 1;
+            try {
+                for (; i <= times; i++) {
+                    BaseCamp red = new PVPCamp(redName, 3);
+                    BaseCamp blue = new PVPCamp(buleName, 3);
+                    for (EntityInfo info : redInfo.infos)
+                        red.addEntity(new ShikigamiEntityImpl(ShikigamiFactory.create(info.shiShen), info.property, MitamaFactory.create(info.mitama), redInfo.lifeTimes, ShikigamiFactory.getDefaultName(info.shiShen)));
+                    for (EntityInfo info : blueInfo.infos)
+                        blue.addEntity(new ShikigamiEntityImpl(ShikigamiFactory.create(info.shiShen), info.property, MitamaFactory.create(info.mitama), blueInfo.lifeTimes, ShikigamiFactory.getDefaultName(info.shiShen)));
+                    Simulator simulator = new Simulator(red, blue, Collections.singletonList(new BattleKoinobori(128.0, red, blue)));
+                    while (simulator.step()) ;
+                    String winName = simulator.getWin().getName();
+                    log.info(Msg.join("测试", i, winName, "胜利"));
+                    count2.put(winName, count2.get(winName) + 1);
+                }
+            } catch (Exception e) {
+                log.severe(JSON.format("test case", i));
+                throw e;
+            }
+            synchronized (count) {
+                count.put(redName, count.get(redName) + count2.get(redName));
+                count.put(buleName, count.get(buleName) + count2.get(buleName));
+            }
+        }
     }
 }
