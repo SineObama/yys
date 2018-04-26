24c3aef67b80546015caa8d2875d4cad4e35391a
Sine <714186139@qq.com>
2018-04-26 16:10:28 +0800

    测试：部分变量和逻辑移至 BaseTest

--- src/test/components/base/BaseTest.java
+++ src/test/components/base/BaseTest.java
@@ -1,4 +1,48 @@
 package test.components.base;
 
-public class BaseTest {
+import com.sine.yys.entity.ShikigamiEntityImpl;
+import com.sine.yys.entity.SimpleObject;
+import com.sine.yys.mitama.BaseMitama;
+import com.sine.yys.shikigami.BaseShikigami;
+import com.sine.yys.shikigami.ShikigamiFactory;
+import com.sine.yys.simulation.PVPCamp;
+import com.sine.yys.simulation.Simulator;
+import test.components.TestProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * 作为基类保有一些变量，提供初始化逻辑。
+ */
+public abstract class BaseTest implements Test {
+    protected final Logger log = Logger.getLogger(this.getClass().getName());
+    protected final PVPCamp c1 = new PVPCamp(redName, 0);
+    protected final PVPCamp c2 = new PVPCamp(buleName, 0);
+    protected final List<ShikigamiEntityImpl> _list1 = new ArrayList<>();
+    protected final List<ShikigamiEntityImpl> _list2 = new ArrayList<>();
+    protected final List<SimpleObject> _extra = new ArrayList<>();
+    protected Simulator simulator;
+
+    protected static ShikigamiEntityImpl form(EnInfo i) {
+        return new ShikigamiEntityImpl(i.shikigami, i.property, i.mitama, 1.0, i.name != null ? i.name : i.shikigami.getName());
+    }
+
+    final void initSimulation() {
+        for (ShikigamiEntityImpl entity : _list1)
+            c1.addEntity(entity);
+        for (ShikigamiEntityImpl entity : _list2)
+            c2.addEntity(entity);
+        simulator = new Simulator(c1, c2, _extra);
+        simulator.init();
+        log.info("初始化模拟完毕");
+    }
+
+    protected class EnInfo {
+        public BaseShikigami shikigami = ShikigamiFactory.create("雨女");
+        public TestProperty property = new TestProperty();
+        public BaseMitama mitama = null;
+        public String name = null;
+    }
 }
--- src/test/components/base/BaseTwoEntityTest.java
+++ src/test/components/base/BaseTwoEntityTest.java
@@ -1,25 +1,13 @@
 package test.components.base;
 
 import com.sine.yys.entity.ShikigamiEntityImpl;
-import com.sine.yys.mitama.BaseMitama;
-import com.sine.yys.shikigami.BaseShikigami;
-import com.sine.yys.shikigami.ShikigamiFactory;
-import com.sine.yys.simulation.BaseCamp;
-import com.sine.yys.simulation.PVPCamp;
-import com.sine.yys.simulation.Simulator;
-import test.components.TestProperty;
-
-import java.util.Collections;
 
 /**
  * 两个式神对打的测试，只有一方出手。
  */
-public abstract class BaseTwoEntityTest implements Test {
+public abstract class BaseTwoEntityTest extends BaseTest implements Test {
     protected final EnInfo i1 = new EnInfo();
     protected final EnInfo i2 = new EnInfo();
-    protected final BaseCamp c1 = new PVPCamp(redName, 0);
-    protected final BaseCamp c2 = new PVPCamp(buleName, 0);
-    protected Simulator simulator;
     protected ShikigamiEntityImpl e1;
     protected ShikigamiEntityImpl e2;
 
@@ -38,18 +26,11 @@ public abstract class BaseTwoEntityTest implements Test {
         i1.property.speed = 10000;
         i2.property.speed = 1;
         init();
-        e1 = new ShikigamiEntityImpl(i1.shikigami, i1.property, i1.mitama, 1.0, i1.shikigami.getName());
-        c1.addEntity(e1);
-        e2 = new ShikigamiEntityImpl(i2.shikigami, i2.property, i2.mitama, 1.0, i2.shikigami.getName());
-        c2.addEntity(e2);
-        simulator = new Simulator(c1, c2, Collections.EMPTY_LIST);
-        simulator.init();
+        e1 = form(i1);
+        _list1.add(e1);
+        e2 = form(i2);
+        _list2.add(e2);
+        initSimulation();
         doTest();
-    }
-
-    protected class EnInfo {
-        public BaseShikigami shikigami = ShikigamiFactory.create("雨女");
-        public TestProperty property = new TestProperty();
-        public BaseMitama mitama = null;
     }
 }
