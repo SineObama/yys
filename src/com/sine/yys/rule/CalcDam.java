4f67c6db2b76c13edc5543064c330cdde83e0776
Sine <714186139@qq.com>
2018-05-04 10:07:37 +0800

    修正注释

--- src/com/sine/yys/rule/CalcDam.java
+++ src/com/sine/yys/rule/CalcDam.java
@@ -17,7 +17,6 @@ public class CalcDam {
 
     /**
      * 计算某式神攻击目标的期望伤害。
-     * 包括暴击的计算（但没有反馈信息）。
      */
     public static double expect(Property self, Property target, AttackInfo attackInfo, boolean critical) {
         final double criticalCoefficient = critical ? self.getCriticalDamage() : 1.0;
