2ecd49dbb9e0af27a629249204b6525b15f73685
Sine <714186139@qq.com>
2018-05-04 10:03:59 +0800

    简化 Seed

--- src/com/sine/yys/util/Seed.java
+++ src/com/sine/yys/util/Seed.java
@@ -1,27 +1,14 @@
 package com.sine.yys.util;
 
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 每次运行静态生产一个随机种子。
  * <p>
- * 种子生产方法来自{@link Random#Random()}。
+ * 种子值等同于首个{@link Random#Random()}的种子。
  */
 public class Seed {
-    private static final AtomicLong seedUniquifier = new AtomicLong(8682522807148012L);
-    private static final long seed = seedUniquifier() ^ System.nanoTime();
-
-    private static long seedUniquifier() {
-        // L'Ecuyer, "Tables of Linear Congruential Generators of
-        // Different Sizes and Good Lattice Structure", 1999
-        for (; ; ) {
-            long current = seedUniquifier.get();
-            long next = current * 181783497276652981L;
-            if (seedUniquifier.compareAndSet(current, next))
-                return next;
-        }
-    }
+    private static final long seed = 8006678197202707420L ^ System.nanoTime();
 
     public static long get() {
         return seed;
